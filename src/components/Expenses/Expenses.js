f83561d3120a09786a97611a6d1bf5bbc44ff65e
ScorchingShade <ankush.sharma.con@hotstar.com>
2022-08-12 09:13:58 +0530

    some improvements

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,39 +1,24 @@
 import React, { useState } from "react";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 function Expenses({ expenses }) {
   const [year, setYear] = useState("2019");
 
-  const filteredExpenses = () => {
-    return expenses.filter(
-      (expense) => expense.date.getFullYear().toString() === year
-    );
-  };
+  const filteredExpenses = expenses.filter(
+    (expense) => expense.date.getFullYear().toString() === year
+  );
 
   const yearHandler = (data) => {
     setYear(data);
   };
 
-  let expenseItems = <p style={{ color: "white" }}>No items found</p>;
-
-  if (filteredExpenses().length > 0) {
-    expenseItems = filteredExpenses()?.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        date={expense.date}
-        title={expense.title}
-        amount={expense.amount}
-      />
-    ));
-  }
-
   return (
     <Card className="expenses">
       <ExpensesFilter yearHandler={yearHandler} selectedYear={year} />
-      {expenseItems}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 }
