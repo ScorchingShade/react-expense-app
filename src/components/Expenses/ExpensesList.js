616644db7b520c42f54cdebd5b110ffdf513480d
ScorchingShade <ankush.sharma.con@hotstar.com>
2022-08-12 09:14:07 +0530

    some improvements

---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./ExpensesList.css";
+import ExpenseItem from "./ExpenseItem";
+
+function ExpensesList({ items }) {
+  if (items?.length === 0) {
+    return <h2 className="expenses-list__fallback">Found no expenses</h2>;
+  }
+
+  return (
+    <ul className="expenses-list">
+      {items?.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          date={expense.date}
+          title={expense.title}
+          amount={expense.amount}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default ExpensesList;
